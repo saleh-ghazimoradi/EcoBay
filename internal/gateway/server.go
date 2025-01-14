dfb480370cdb12ea3bb6c777ecdf2758eb2c9038
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-14 21:05:03 +0330

    execute the DBConnection in the server

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -3,14 +3,24 @@ package gateway
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/saleh-ghazimoradi/EcoBay/config"
+	"github.com/saleh-ghazimoradi/EcoBay/internal/gateway/rest/routes"
+	"github.com/saleh-ghazimoradi/EcoBay/utils"
 )
 
 func Server() error {
 	app := fiber.New()
 
-	if err := app.Listen(config.AppConfig.ServerConfig.Port); err != nil {
+	_, err := utils.DBConnection(utils.DBMigrator)
+	if err != nil {
 		return err
 	}
-	
+
+	routes.Health(app)
+	routes.UserRoutes(app)
+
+	if err = app.Listen(config.AppConfig.ServerConfig.Port); err != nil {
+		return err
+	}
+
 	return nil
 }
