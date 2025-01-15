50a4812895c192bea1a637148d44ec2492ba13cd
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-15 17:41:52 +0330

    implement user functionality in the repository layer

---
+++ internal/repository/user.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"github.com/saleh-ghazimoradi/EcoBay/internal/service/service_models"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, user *service_models.User) (*service_models.User, error)
+	FindUserByEmail(ctx context.Context, email string) (*service_models.User, error)
+	FindUserByID(ctx context.Context, id uint) (*service_models.User, error)
+	UpdateUser(ctx context.Context, id uint, user *service_models.User) (*service_models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func (u *userRepository) CreateUser(ctx context.Context, user *service_models.User) (*service_models.User, error) {
+	if err := u.db.WithContext(ctx).Create(&user).Error; err != nil {
+		return nil, errors.New("create user failed")
+	}
+	return user, nil
+}
+
+func (u *userRepository) FindUserByEmail(ctx context.Context, email string) (*service_models.User, error) {
+	var user service_models.User
+	if err := u.db.WithContext(ctx).First(&user, "email = ?", email).Error; err != nil {
+		return nil, errors.New("user does not exist")
+	}
+
+	return &user, nil
+}
+
+func (u *userRepository) FindUserByID(ctx context.Context, id uint) (*service_models.User, error) {
+	var user service_models.User
+	if err := u.db.WithContext(ctx).First(&user, id).Error; err != nil {
+		return nil, errors.New("user does not exist")
+	}
+
+	return &user, nil
+}
+
+func (u *userRepository) UpdateUser(ctx context.Context, id uint, user *service_models.User) (*service_models.User, error) {
+	if err := u.db.WithContext(ctx).Model(&user).Clauses(clause.Returning{}).Where("id = ?", id).Updates(user).Error; err != nil {
+		return nil, errors.New("error updating user")
+	}
+	return user, nil
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
